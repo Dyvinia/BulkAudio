d0f382ba0af2d3bc43e1e16ee8c0c1e66f24ca01
Dyvine57 <dulana57@gmail.com>
2022-01-25 03:30:11 -0500

    clean

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,17 +8,9 @@ using System.IO;
 using System.Linq;
 using System.Media;
 using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 
 namespace BulkAudio {
     /// <summary>
